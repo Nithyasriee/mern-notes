38d2d44769a8acbf685e471f9682a7485270c631
Nithya <nithyasriee2004@gmail.com>
2025-09-03 00:15:41 +0530

    Fix environment variable issue

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,10 @@
 import axios from "axios";
-const BASE_URL=import.meta.end.MODE==="development" ? "http://localhost:5001/api" : "/api";
+
+const BASE_URL ="https://memoir-notes.onrender.com/api";
+
+
 const api = axios.create({
-    baseURL: "BASE_URL",
-}); 
+  baseURL: BASE_URL,
+});
+
 export default api;
